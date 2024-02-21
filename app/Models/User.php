65524c67ea1a06b4823228a1930e2d4de81de241
Jhonatan <7jhon2898@gmail.com>
2024-02-21 14:14:27 -0500

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'last_name',
+        'mother_last_name',
         'email',
         'password',
     ];
